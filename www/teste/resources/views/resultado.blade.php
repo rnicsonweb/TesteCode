83ac0525e846ba2bd2ba776ef239d306cd7310ef
Richard Nicson Costa de Lima <rnicsonweb@gmail.com>
2021-09-01 20:15:01 -0300

    insert

---
+++ www/teste/resources/views/resultado.blade.php
@@ -0,0 +1,13 @@
+<style>
+div {
+ margin-top: 15%;
+}
+</style>
+<body>
+<div align="center">
+<p><h1>Time1</h1> <h3>{{ $conversao }}</h3></p>
+
+<hr>
+<p><h1>Time2</h1> <h3>{{ $conversao2 }}</h3></p>
+</div>
+</body>
